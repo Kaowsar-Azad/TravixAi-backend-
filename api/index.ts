78e75c2b1143f5fe5a00d72e7ba56d9506d70aff
Pranto800 <kaowsarazad33@gmail.com>
2026-07-20 18:22:13 +0600

    add .js extension to satisfy strict ESM resolution

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import app from '../src/server';
+import app from '../src/server.js';
 
 export default (req: Request, res: Response) => {
   return app(req, res);
